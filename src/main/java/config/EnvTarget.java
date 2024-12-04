4a1373e35c1570452373eb4683673c53c02ee4a4
Aji Fauzi P <ajifauzi919@gmail.com>
2024-12-04 17:31:00 +0700

    add environment testing

--- src/main/java/config/EnvTarget.java
+++ src/main/java/config/EnvTarget.java
@@ -1,4 +1,8 @@
 package config;
 
+import org.openqa.selenium.WebDriver;
+
 public class EnvTarget {
+    protected WebDriver driver;
+    protected String baseUrl = "https://parabank.parasoft.com/parabank/index.htm";
 }
